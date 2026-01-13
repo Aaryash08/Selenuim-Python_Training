72777a33b8abff44456939530b6f07281b1ded9c
Aaryash08 <113716203+Aaryash08@users.noreply.github.com>
2026-01-13 15:52:43 +0530

    Add files via upload

---
+++ 13-01/TC_BuildInFunction.py
@@ -0,0 +1,16 @@
+import math
+print(math.sqrt(9))
+print(math.pow(2,3))
+print(math.log(4,100))
+print(math.gamma(100))
+
+a=20
+b=30
+if(a==b):
+    print("equal")
+else:
+    print("Not equal")
+if(a!=b):
+    print("Not equal")
+else:
+    print("equal")
